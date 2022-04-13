e19c959a3222254ad8d3c9a8460eefb674135873
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2022-04-13 13:18:33 -0500

    Use `package_dirs` with `find:` and `include` (#10)

--- setup_builder.py
+++ setup_builder.py
@@ -165,13 +165,10 @@ class OptionsSection(Section):
     """Encapsulates the *minimal* `[options]` section & checks for required/invalid fields."""
 
     python_requires: str
-    packages: str
     install_requires: str
+    packages: str
 
     def __post_init__(self) -> None:
-        if not self.packages.strip():
-            self.packages = "find:"
-
         # sort requirements if they're dangling
         if "\n" in self.install_requires.strip():
             as_lines = self.install_requires.strip().split("\n")
@@ -404,16 +401,23 @@ def _build_out_sections(
         cfg["options"] = {}
     osec = OptionsSection(
         python_requires=bsec.python_requires(),
-        packages=list_to_dangling(bsec.packages()),
+        packages="find:",  # always use "find:", then use include/exclude
         install_requires=cfg["options"].get("install_requires", fallback=""),
     )
     cfg["options"] = osec.add_unique_fields(dict(cfg["options"]))
 
     # [options.packages.find]
     if cfg["options"]["packages"] == "find:":
-        cfg["options.packages.find"] = {
-            "exclude": list_to_dangling(DEFAULT_DIRECTORY_EXCLUDE),
-        }
+        pkgs = bsec.packages()
+        cfg["options.packages.find"] = {}
+        if pkgs:
+            cfg["options.packages.find"]["include"] = list_to_dangling(
+                pkgs + [f"{p}.*" for p in pkgs]
+            )
+        else:
+            cfg["options.packages.find"]["exclude"] = list_to_dangling(
+                DEFAULT_DIRECTORY_EXCLUDE
+            )
 
     # [options.package_data]
     if not cfg.has_section("options.package_data"):  # will only override some fields
