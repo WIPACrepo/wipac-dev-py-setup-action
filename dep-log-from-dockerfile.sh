baf4c82eb31718e23d0b6653e06dc63fdef3310e
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2023-11-07 13:48:58 -0600

    Add `dockerfile_nametags` [minor] (#49)

--- dep-log-from-dockerfile.sh
+++ dep-log-from-dockerfile.sh
@@ -9,18 +9,21 @@ set -e
 ########################################################################
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "Usage: dep-log-from-dockerfile.sh DOCKERFILE DEPS_LOG_FILE SUBTITLE"
+    echo "Usage: dep-log-from-dockerfile.sh DOCKERFILE DEPS_LOG_FILE SUBTITLE [IMAGE_NAMETAG]"
     exit 1
 fi
 if [ ! -f "$1" ]; then
     echo "File Not Found: $1"
     exit 2
 fi
-DEPS_LOG_FILE=$2
-SUBTITLE=$3
-
-# lower basename without extension
-image="for-deps-$(echo $(basename ${DEPS_LOG_FILE%.*}) | awk '{print tolower($0)}')"
+DEPS_LOG_FILE="$2"
+SUBTITLE="$3"
+if [ -z "$4" ]; then  # optional -> get default
+    # lower basename without extension
+    image="for-deps-$(echo $(basename ${DEPS_LOG_FILE%.*}) | awk '{print tolower($0)}')"
+else
+    image="$4"
+fi
 
 
 # move script
@@ -31,23 +34,25 @@ chmod +x $TEMPDIR/pip-freeze-tree.sh
 
 
 # build & generate
-if [[ $* == *--podman* ]]; then
+if [[ $* == *--podman* ]]; then  # look for flag anywhere in args
     podman build -t $image --file $1 .
     podman run --rm -i \
         --env PACKAGE_NAME=$PACKAGE_NAME \
         --env ACTION_REPOSITORY=$ACTION_REPOSITORY \
+        --env SUBTITLE="$SUBTITLE" \
         --mount type=bind,source=$(realpath $TEMPDIR/),target=/local/$TEMPDIR \
         --userns=keep-id:uid=1000,gid=1000 \
         $image \
-        /local/$TEMPDIR/pip-freeze-tree.sh /local/$TEMPDIR/$DEPS_LOG_FILE "$SUBTITLE"
+        /local/$TEMPDIR/pip-freeze-tree.sh /local/$TEMPDIR/$DEPS_LOG_FILE
 else
     docker build -t $image --file $1 .
     docker run --rm -i \
         --env PACKAGE_NAME=$PACKAGE_NAME \
         --env ACTION_REPOSITORY=$ACTION_REPOSITORY \
+        --env SUBTITLE="$SUBTITLE" \
         --mount type=bind,source=$(realpath $TEMPDIR/),target=/local/$TEMPDIR \
         $image \
-        /local/$TEMPDIR/pip-freeze-tree.sh /local/$TEMPDIR/$DEPS_LOG_FILE "$SUBTITLE"
+        /local/$TEMPDIR/pip-freeze-tree.sh /local/$TEMPDIR/$DEPS_LOG_FILE
 fi
 
 ls $TEMPDIR
