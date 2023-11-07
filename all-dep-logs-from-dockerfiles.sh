dcc62dd57dccfc57dfd8a6e79dead6abfda23ec5
Ric Evans <ric@evans-jacquez.com>
2023-11-07 13:41:51 -0600

    pt 2

--- all-dep-logs-from-dockerfiles.sh
+++ all-dep-logs-from-dockerfiles.sh
@@ -26,7 +26,7 @@ for fname_nametag in $DOCKERFILE_NAMETAGS; do
     $GITHUB_ACTION_PATH/dep-log-from-dockerfile.sh \
         $fname \
         "dependencies-from-$(basename $fname).log" \
-        "within the container built from \`$fname\`" \
+        "within the container built from '$fname'" \
         $nametag \
         $USE_PODMAN
 done
