8b495429f2c8f0e8d0bd2277b190e3657b2e8b6c
Ric Evans <ric@evans-jacquez.com>
2023-11-14 11:46:25 -0600

    use var

--- install-podman.sh
+++ install-podman.sh
@@ -20,14 +20,16 @@ sudo mkdir -p /etc/apt/keyrings
 #     https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Testing/ /" \
 #   | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
 
+OS_RELEASE='Debian_Unstable'
+
 # Debian Unstable/Sid
-# curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/Release.key \
-#   | gpg --dearmor \
-#   | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-#     https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/ /" \
-#   | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/$OS_RELEASE/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/$OS_RELEASE/ /" \
+  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
 
 # Install Podman
 sudo apt-get update
