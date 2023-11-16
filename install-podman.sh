3af3dbd47dd4c890d61ece30ecddf6167f811696
Ric Evans <ric@evans-jacquez.com>
2023-11-16 13:07:57 -0600

    try using `:/`

--- install-podman.sh
+++ install-podman.sh
@@ -26,7 +26,7 @@ curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:
   | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/ /" \
+    https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/Debian_Unstable/ /" \
   | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
 
 # Install Podman
