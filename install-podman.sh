ae9202ab2b7ad76b14a031889db4da66011d9d3e
Ric Evans <ric@evans-jacquez.com>
2023-11-16 13:13:22 -0600

    comment and `set -x`

--- install-podman.sh
+++ install-podman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -x  # turn on debugging
+set -x  # turn on debugging
 set -e
 
 ########################################################################
@@ -24,6 +24,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/Release.key \
   | gpg --dearmor \
   | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+# -> see https://github.com/openSUSE/MirrorCache/issues/428#issuecomment-1814992424
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
     https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/Debian_Unstable/ /" \
