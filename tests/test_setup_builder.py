b6b5a71354dd90ea40fa2fd0b8ecceefef1f674a
ric-evans <emejqz@gmail.com>
2024-03-22 10:36:33 -0500

    use all readable ascii chars

--- tests/test_setup_builder.py
+++ tests/test_setup_builder.py
@@ -79,15 +79,9 @@ telemetry = [
 ]
 """
 
-PATCH_WITHOUT_TAG_WORKAROUND = """
-# allow patch releases without specified commit tags (patch_without_tag=True)
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", 
-    "j", "k", "l", "m", "n", "o", "p", "q", "r", 
-    "s", "t", "u", "v", "w", "x", "y", "z", " ",
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", 
-    "J", "K", "L", "M", "N", "O", "P", "Q", "R", 
-    "S", "T", "U", "V", "W", "X", "Y", "Z", ".",
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+PATCH_WITHOUT_TAG_WORKAROUND = f"""
+    # allow patch releases without specified commit tags (patch_without_tag=True)
+    {str(chr(i) for i in range(32, 127)).lstrip("[").rstrip("]")}
 """
 
 VANILLA_SEMANTIC_RELEASE_SECTION = f"""
