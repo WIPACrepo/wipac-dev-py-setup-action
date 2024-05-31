71daa06b40264bfda752798da76f22595882f607
ric-evans <emejqz@gmail.com>
2024-05-31 12:56:04 -0400

    update patch workaround - 3

--- tests/test_pyproject_toml_builder.py
+++ tests/test_pyproject_toml_builder.py
@@ -82,11 +82,11 @@ PYPI_URLS_KEYVALS = {
 }
 
 # allow patch releases without specified commit tags (patch_without_tag=True)
+# fmt: off
 PATCH_WITHOUT_TAG_WORKAROUND = [
-    chr(i)
-    for i in range(32, 127)
-    if chr(i) not in ['"', ",", "\\"]  # else upsets toml syntax
+    " ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~",
 ]
+# fmt: on
 
 VANILLA_PROJECT_KEYVALS = {
     **VANILLA_SECTIONS_IN["project"],
