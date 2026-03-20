f0c30c23c0c890405ad890927f4eddf4936de07f
ric-evans <emejqz@gmail.com>
2026-03-20 16:45:28 -0500

    no sub

--- augment_readme.py
+++ augment_readme.py
@@ -122,7 +122,7 @@ class HeaderAugmenter:
             "\n\n",
             f"> {self.gh_api.description.strip()}",
             "\n\n",
-            f"<sub>{self.gh_api.full_repo}` · {', '.join(self.keywords)}</sub>",
+            f"_{self.gh_api.full_repo}_ · {', '.join(self.keywords)}",
             "\n\n",
             self.END_DELIMITER,
             "\n",  # only one newline here, otherwise we get an infinite commit-loop
